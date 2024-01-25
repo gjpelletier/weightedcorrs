e37fc7c4b9650618961b57f4b03f66ab7b1b0ebf
Greg Pelletier <gjpelletier@users.noreply.github.com>
2024-01-24 17:50:01 -0800

    Add files via upload

--- weightedcorrs.m
+++ weightedcorrs.m
@@ -1,5 +1,5 @@
 
-function [R,p,wcov,wstd] = weightedcorrs(Y, w)
+function [R,p,wcov,wstd,wmean] = weightedcorrs(Y, w)
 
 %	Weighted correlation coefficients, covariance, and standard deviations
 %
@@ -9,6 +9,7 @@ function [R,p,wcov,wstd] = weightedcorrs(Y, w)
 % 		[~,p] = weightedcorrs(Y,w)				% returns p-values of the weighted correlation coefficients
 % 		[~,~,wcov] = weightedcorrs(Y,w)			% returns weighted covariance of Y weighted by w normalized to number of observations (nobs)
 % 		[~,~,~,wstd] = weightedcorrs(Y,w)		% returns weighted standard deviations of Y weighted by w normalized to nobs
+% 		[~,~,~,~,wmean] = weightedcorrs(Y,w)	% returns weighted means Y weighted by w
 % 		[~,~,wcov,wstd] = weightedcorrs(Y)		% returns unweighted covariance and standard deviations normalized to nobs-1 (same as std(Y) and cov(Y))
 % 		[~,~,wcov,wstd] = weightedcorrs(Y,0)	% returns unweighted covariance and standard deviations normalized to nobs-1 (same as std(Y,0) and cov(Y,0))
 % 		[~,~,wcov,wstd] = weightedcorrs(Y,1)	% returns unweighted covariance and standard deviations normalized to nobs (same as std(Y,1) and cov(Y,1)
@@ -57,7 +58,7 @@ function [R,p,wcov,wstd] = weightedcorrs(Y, w)
 % load hospital
 % X = [hospital.Weight hospital.BloodPressure];
 % w = hospital.Age;
-% [wrho,p,wcov,wstd] = weightedcorrs(X,w)   
+% [wrho,p,wcov,wstd,wmean] = weightedcorrs(X,w)   
 % % wrho =
    % % 1.0000e+00   1.5541e-01   2.3071e-01
    % % 1.5541e-01   1.0000e+00   5.1037e-01
@@ -72,16 +73,18 @@ function [R,p,wcov,wstd] = weightedcorrs(Y, w)
    % % 4.1788e+01   2.3918e+01   4.7989e+01
 % % wstd =
    % % 2.6146e+01   6.7651e+00   6.9274e+00
+% % wmean =
+   % % 1.5445e+02   1.2295e+02   8.3064e+01
 % % check compared with matlab std for weighted std
-% wstd_ = std(X,w)
-% wstd_ =
+% wstd = std(X,w)
+% % wstd =
 % %    2.6146e+01   6.7651e+00   6.9274e+00
 % % - - -
 % % EXAMPLE USE of weightedcorrs.m without weighting 
 % % (same as matlab corrcoef, cov, std unweighted)
 % load hospital
 % X = [hospital.Weight hospital.BloodPressure];
-% [wrho,p,wcov,wstd] = weightedcorrs(X)   
+% [wrho,p,wcov,wstd,wmean] = weightedcorrs(X)   
 % % wrho =
    % % 1.0000e+00   1.5579e-01   2.2269e-01
    % % 1.5579e-01   1.0000e+00   5.1184e-01
@@ -96,9 +99,11 @@ function [R,p,wcov,wstd] = weightedcorrs(Y, w)
    % % 4.1020e+01   2.3819e+01   4.8059e+01
 % % wstd =
    % % 2.6571e+01   6.7128e+00   6.9325e+00
+% % wmean =
+   % % 1.5400e+02   1.2278e+02   8.2960e+01
 % % check compared with matlab corrcoef for unweighted correlation
-% [R,p] = corrcoef(X)
-% % R =
+% [rho,p] = corrcoef(X)
+% % rho =
    % % 1.0000e+00   1.5579e-01   2.2269e-01
    % % 1.5579e-01   1.0000e+00   5.1184e-01
    % % 2.2269e-01   5.1184e-01   1.0000e+00
@@ -107,14 +112,14 @@ function [R,p,wcov,wstd] = weightedcorrs(Y, w)
    % % 1.2168e-01   1.0000e+00   5.2460e-08
    % % 2.5953e-02   5.2460e-08   1.0000e+00
 % % check compared with matlab cov for unweighted covariance
-% covx = cov(X)
-% % covx =
+% uwcov = cov(X)
+% % uwcov =
    % % 7.0604e+02   2.7788e+01   4.1020e+01
    % % 2.7788e+01   4.5062e+01   2.3819e+01
    % % 4.1020e+01   2.3819e+01   4.8059e+01
 % % check compared with matlab std for unweighted std
-% stdx = std(X)
-% % stdx =
+% uwstd = std(X)
+% % uwstd =
    % % 2.6571e+01   6.7128e+00   6.9325e+00
 
 %
@@ -150,20 +155,21 @@ if ~ctrl
 end
 
 [nobs, nvar] = size(Y);                                                       % nobs: number of observations; nvar: number of variables
-temp = Y - repmat(w' * Y, nobs, 1);                                           % remove mean (which is, also, weighted)
-temp = temp' * (temp .* repmat(w, 1, nvar));                                  % weighted covariance matrix normalized nobs
+wmean = w' * Y;                                                               % weighted means of X
+temp = Y - repmat(w' * Y, nobs, 1);                                           % center X by remove weighted means
+temp = temp' * (temp .* repmat(w, 1, nvar));                                  % weighted covariance matrix normalized to nobs
 
 wstd = sqrt(diag(temp))';													  % weighted standard deviations normalized to nobs
-% standard deviations adjusted to df=nobs-1 if w=0 or missing
+% standard deviations normalized to nobs-1 if w=0 or missing
 if ~df_eq_nobs
 	wstd = wstd .* sqrt(nobs / (nobs-1));									  % normalize to nobs-1 if w=0 or missing
 end
 
 temp = 0.5 * (temp + temp');                                                  % Must be exactly symmetric
-R = diag(temp);                                                               % weighted variances (df=nobs)
+R = diag(temp);                                                               % weighted variances normalized to nobs
 R = temp ./ sqrt(R * R');                                                     % Matrix of Weighted Correlation Coefficients
 
-% weighted covariance matrix (df=nobs-1 if w=0 or missing, otherwise df=nobs)
+% weighted covariance matrix (normalized to nobs-1 if w=0 or missing, otherwise to nobs)
 D = diag(wstd);																  % diagonal matrix of weighted standard deviations 
 wcov = D*R*D;															  	  % weighted covariance matrix 
 
